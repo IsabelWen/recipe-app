4ed0c517f3d515bc500b12b20ac4ccca8e7aefde
Isy <isy1997@freenet.de>
2024-03-21 19:33:28 +0100

    Implement auth

--- src/recipes/views.py
+++ src/recipes/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Recipe
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 def home(request):
    return render(request, 'recipes/recipes_home.html')
 
-class RecipesListView(ListView):
+class RecipesListView(LoginRequiredMixin, ListView):
    model = Recipe
    template_name = 'recipes/recipes_list.html'
 
-class RecipesDetailView(DetailView):
+class RecipesDetailView(LoginRequiredMixin, DetailView):
    model = Recipe
    template_name = 'recipes/recipes_details.html'
--- src/users/views.py
+++ src/users/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from .models import User
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def profile(request):
     user = User.objects.first()  # Retrieve the first user
     context = {'user': user}
