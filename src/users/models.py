3795f30373b98b293a8345befa5b386b7b610c59
Isy <isy1997@freenet.de>
2024-03-22 17:35:30 +0100

    Improvements based on mentor recommendations

--- src/users/models.py
+++ src/users/models.py
@@ -2,12 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class User(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
+class CustomUser(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=120)
-    username = models.CharField(max_length=120)
     pic = models.ImageField(upload_to='users', default='no_picture.jpg')
-    bio = models.TextField(default="No bio...")
+    bio = models.TextField(default="No bio ...")
 
     def __str__(self):
-        return f"{self.username}"
+        return f"{self.name}"
+    
+    # creates a user object with the same username if it doesn't exist
+    def save(self, *args, **kwargs):
+        if not self.user:
+            user = User.objects.get_or_create(
+                username=self.username,
+            )
+            self.user = user
+        super().save(*args, **kwargs)
--- src/users/views.py
+++ src/users/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
-from .models import User
+from .models import CustomUser
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import DetailView
 
 # Create your views here.
 class profile(LoginRequiredMixin, DetailView):
-    model: User
+    model: CustomUser
     template_name = 'users/users_profile.html'
     context_object_name = 'current_user'
     def get_object(self, queryset=None):
-        return self.request.user
+        return CustomUser.objects.get(user=self.request.user)
