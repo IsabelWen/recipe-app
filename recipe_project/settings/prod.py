d893d1a611ab435cdd7e7bc426e595770f96d248
Isy <isy1997@freenet.de>
2024-03-29 19:42:29 +0100

    Update production settings

--- recipe_project/settings/prod.py
+++ recipe_project/settings/prod.py
@@ -33,8 +33,8 @@ STATICFILES_DIRS = [
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
-STORAGES = {
-    'staticfiles': {
-        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
-    }
-}
+#STORAGES = {
+#    'staticfiles': {
+#        'BACKEND': 'whitenoise.storage.CompressedManifestStaticFilesStorage',
+#    }
+#}
