45731310d3bf06b06e33fa37942eb3c5f46bfe95
Isy <isy1997@freenet.de>
2024-03-29 20:16:22 +0100

    Update urls.py

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from .views import home
 from .views import RecipesListView, RecipesDetailView, RecipesAuthorView, create_recipe, update_recipe, delete_recipe, about
+from django.urls import re_path
+from django.conf import settings
+from django.views.static import serve
 
 app_name = 'recipes'  
 
@@ -13,4 +16,5 @@ urlpatterns = [
    path("create/", create_recipe, name="create_recipe"),
    path("update/<int:pk>/", update_recipe, name="update_recipe"),
    path("delete/<int:pk>/", delete_recipe, name="delete_recipe"),
+   re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
