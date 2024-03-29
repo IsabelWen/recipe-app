c532f9b309dfa03bc1a3ac70574b1c4ce751c2b2
Isy <isy1997@freenet.de>
2024-03-29 16:56:38 +0100

    Relocate static folder

--- recipe_project/settings/prod.py
+++ recipe_project/settings/prod.py
@@ -18,7 +18,7 @@ DATABASES = {
     'default': dj_database_url.config(
         # Replace this value with your local database's connection string.
         default=env.str('DATABASE_URL'),
-        conn_max_age=600
+        conn_max_age=500
     )
 }
 
@@ -29,7 +29,8 @@ MEDIA_ROOT= BASE_DIR / 'media'
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS=[
-   BASE_DIR / 'recipes' / 'static'
+   BASE_DIR / 'static'
 ]
+
 # The absolute path to the directory where collectstatic will collect static files for deployment.
 STATIC_ROOT = BASE_DIR /  'staticfiles'
